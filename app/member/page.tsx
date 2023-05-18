5682267eda22cf160f80dc47b5f234b3b61c3f51
Joghur <joghur@gmail.com>
2023-05-18 15:49:44 +0200

    Fix deploy

--- app/member/page.tsx
+++ app/member/page.tsx
@@ -1,10 +1,4 @@
-'use client';
-
-import {isMobile} from '@utils/sizes';
-
 const MemberPage = () => {
-  const isLarge = isMobile();
-  console.log(isLarge);
   return (
     <>
       <p>Med-lems side</p>
