8f7585953b43ec4ad10e775346a821c623f95dfe
Joghur <joghur@gmail.com>
2023-05-18 15:10:00 +0200

    Lint

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -3,7 +3,6 @@ import Profile from '@components/member/Profile';
 const ProfilePage = () => {
   return (
     <>
-    
       <Profile
         name="My"
         desc="Welcome to your personalized profile page. Share your exceptional prompts and inspire others with the power of your imagination"
