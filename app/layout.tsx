20c8579041d80c7073ca62824994b0dc16660f41
Joghur <joghur@gmail.com>
2023-05-18 16:57:47 +0200

    Meta 2

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,6 @@ import BottomNav from '@components/layout/BottomNav';
 export const metadata = {
   title: 'IQ96 web app',
   description: 'IQ96 web app',
-  content:'width=device-width, initial-scale=1',
 };
 
 interface Props {
@@ -16,11 +15,14 @@ interface Props {
 
 const RootLayout = ({children}: Props) => (
   <html lang="en">
+    <head>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link rel="icon" href="/favicon.ico" />
+    </head>
     <body>
       <div className="main">
         <div className="gradient" />
       </div>
-
       <main className="app">
         <HeaderNav />
         {children}
