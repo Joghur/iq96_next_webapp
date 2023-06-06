ccd7846b9313735ffe407b10b22d12e81f789c27
Joghur <joghur@gmail.com>
2023-06-06 12:24:58 +0200

    Button bug fix

--- components/ui/OldPageButton.tsx
+++ components/ui/OldPageButton.tsx
@@ -2,7 +2,7 @@ const OldPageButton = () => {
   return (
     <button
       className="dynamic_text btn-outline btn rounded-xl"
-      onClick={() => (window.location.href = "https://www.iq96.dk")}
+      onClick={() => (window.location.href = "https://www.iq96.dk/IQ3")}
     >
       Gå til den gamle side iq96.dk
     </button>
