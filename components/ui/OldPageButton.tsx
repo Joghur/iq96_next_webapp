524e36e4e8d9b3f7bdd8351ce46bf91c11bc815b
Joghur <joghur@gmail.com>
2023-06-06 12:43:55 +0200

    Fixed button styling

--- components/ui/OldPageButton.tsx
+++ components/ui/OldPageButton.tsx
@@ -1,12 +1,12 @@
 const OldPageButton = () => {
   return (
     <button
-      className="dynamic_text btn-outline btn rounded-xl"
+      className="dynamic_text btn-outline btn rounded-xl lg:text-xs"
       onClick={() =>
         (window.location.href = process.env.NEXT_PUBLIC_OLDPAGE_LINK || "")
       }
     >
-      Gå til den gamle side - {process.env.NEXT_PUBLIC_OLDPAGE}
+      Gå til den gamle side {process.env.NEXT_PUBLIC_OLDPAGE}
     </button>
   );
 };
