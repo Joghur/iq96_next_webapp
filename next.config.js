58cfa3c7eb42e814ebe96b87b56bf8749bbc251b
Joghur <joghur@gmail.com>
2023-09-04 10:04:11 +0200

    Cloudinary production error

--- next.config.js
+++ next.config.js
@@ -3,6 +3,11 @@ const nextConfig = {
   experimental: {
     serverActions: true,
   },
+  images: {
+    loader: 'cloudinary',
+    domains: ['res.cloudinary.com'],
+    path: 'https://res.cloudinary.com/%22dn01o52nc%22/image/upload',
+  },
 };
 
 module.exports = nextConfig;
